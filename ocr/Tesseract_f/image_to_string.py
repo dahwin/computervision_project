f7cab2e08c6af7bc234c6299ad984b869ab96f3f
dahwin <116961692+dahwin@users.noreply.github.com>
2023-11-29 06:45:51 +0600

    cv

--- ocr/Tesseract_f/image_to_string.py
+++ ocr/Tesseract_f/image_to_string.py
@@ -4,12 +4,12 @@ import pytesseract
 import time
 
 start = time.time()
-pytesseract.pytesseract.tesseract_cmd = r"C:\\Program Files\\Tesseract-OCR\\tesseract"
-text  = pytesseract.image_to_string('fiverr1-.png').lower()
-input = 'ashmawy1996'.lower()
-if input in text:
-    print(True)
-end = time.time()
-print(end-start)
-
+pytesseract.pytesseract.tesseract_cmd = r"C:\Users\ALL USER\Desktop\computervision_project\Tesseract-OCR\tesseract.exe"
+text  = pytesseract.image_to_string('dahyun.jpg').lower()
+# input = 'ashmawy1996'.lower()
+# if input in text:
+#     print(True)
+# end = time.time()
+# print(end-start)
+print(text)
 
